5326e883e4ea6824ddfb585da622d5efa6505c62
Taro Altricher <ta029@hdm-stuttgart.de>
2024-06-26 14:41:18 +0200

    react router setup

--- AnimeCollection/src/App.jsx
+++ AnimeCollection/src/App.jsx
@@ -1,6 +1,16 @@
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from "react-router-dom";
 import AnimeCollection from "./components/AnimeCollection";
 import Navbar from "./components/Navbar";
 import ViewCollection from "./components/ViewCollection";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={<h1>App</h1>} />)
+);
 
 const App = () => {
     return (
