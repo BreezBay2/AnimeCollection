726367f2ba6fab1bc27310dbc90fa28edbd6093d
Taro Altricher <ta029@hdm-stuttgart.de>
2024-06-26 14:34:54 +0200

    fetch data function

--- AnimeCollection/src/components/AnimeCollection.jsx
+++ AnimeCollection/src/components/AnimeCollection.jsx
@@ -1,6 +1,23 @@
 import AnimeItem from "./AnimeItem";
+import { useEffect, useState } from "react";
 
 const AnimeCollection = () => {
+    const [collection, setCollection] = useState([]);
+
+    useEffect(() => {
+        const fetchCollection = async () => {
+            try {
+                const res = await fetch("http://localhost:8000/collection");
+                const data = await res.json();
+                setCollection(data);
+            } catch {
+                console.log("Error fetching data", error);
+            }
+        };
+
+        fetchCollection();
+    }, []);
+
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
@@ -8,9 +25,9 @@ const AnimeCollection = () => {
                     Anime Collection
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    <AnimeItem />
-                    <AnimeItem />
-                    <AnimeItem />
+                    {collection.map((anime) => (
+                        <AnimeItem key={anime.id} anime={anime} />
+                    ))}
                 </div>
             </div>
         </section>
